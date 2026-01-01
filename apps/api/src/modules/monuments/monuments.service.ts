c1c4c9b0850abd6d7c9497bf3c83ff2d44d49dba
Mohamed Abdel Moneim <Eachawy@gmail.com>
2026-01-01 05:09:20 +0400

    Fix issue

--- apps/api/src/modules/monuments/monuments.service.ts
+++ apps/api/src/modules/monuments/monuments.service.ts
@@ -19,6 +19,7 @@ export class MonumentsService {
         monumentType: true,
         era: true,
         dynasty: true,
+        monumentDescriptions: true,
       },
       orderBy: {
         id: 'desc',
